460aca403c1045b70e423efb0b5e5d4ce3cc3df8
Sudip Shrestha <71261804+Sudip2073@users.noreply.github.com>
2022-03-18 00:45:55 +0545

    Update QR.php

--- QR.php
+++ QR.php
@@ -83,6 +83,7 @@ $con=mysqli_connect("localhost","root","","myhmsdb");
     <h3 style = "margin-left: 40%;  padding-bottom: 20px; font-family: 'IBM Plex Sans', sans-serif;"> Here is your QR code &nbsp<?php echo $username ?> 
    </h3>
    <h4 style = "margin-left: 50%;  padding-bottom: 20px; font-family: 'IBM Plex Sans', sans-serif;">Patient ID=<?php echo $pid ?></h4> 
+   <h4 style="margin-left: 48%;  padding-bottom: 20px; font-family: 'IBM Plex Sans', sans-serif;">Scan for verification</h4>
 
     <div class="row">
   <div class="col-md-4" style="max-width:25%; margin-top: 3%">
@@ -100,19 +101,15 @@ $con=mysqli_connect("localhost","root","","myhmsdb");
               <div class="row">
                
 
-                <div class="col-sm-4" style="left: 10%">
+                <div class="col-sm-4" style=" margin-left: 200px;">
                   <div class="panel panel-white no-radius text-center">
                     <div class="panel-body" >
-                      <span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-paperclip fa-stack-1x fa-inverse"></i> </span>
-                      <h4 class="StepTitle" style="margin-top: 5%;">Scan for verification</h2>
+                      <img src="images\QR.png" alt="Flowers in a" width="460" height="345">
+                      
                     </div>
                   </div>
                 </div>
-                </div>
-
-                
-                
-         
+                </div>         
             </div>
           </div>
 
@@ -122,313 +119,20 @@ $con=mysqli_connect("localhost","root","","myhmsdb");
 
       <div class="tab-pane fade" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
         <div class="container-fluid">
-          <div class="card">
-            <div class="card-body">
-              <center><h4>Create an appointment</h4></center><br>
-              <form class="form-group" method="post" action="admin-panel.php">
-                <div class="row">
-                  
-                  <!-- <?php
-
-                        $con=mysqli_connect("localhost","root","","myhmsdb");
-                        $query=mysqli_query($con,"select username,spec from doctb");
-                        $docarray = array();
-                          while($row =mysqli_fetch_assoc($query))
-                          {
-                              $docarray[] = $row;
-                          }
-                          echo json_encode($docarray);
-
-                  ?> -->
-        
-
-                    <div class="col-md-4">
-                          <label for="spec">Specialization:</label>
-                        </div>
-                        <div class="col-md-8">
-                          <select name="spec" class="form-control" id="spec">
-                              <option value="" disabled selected>Select Specialization</option>
-                              <?php 
-                              display_specs();
-                              ?>
-                          </select>
-                        </div>
-
-                        <br><br>
-
-                        <script>
-                      document.getElementById('spec').onchange = function foo() {
-                        let spec = this.value;   
-                        console.log(spec)
-                        let docs = [...document.getElementById('doctor').options];
-                        
-                        docs.forEach((el, ind, arr)=>{
-                          arr[ind].setAttribute("style","");
-                          if (el.getAttribute("data-spec") != spec ) {
-                            arr[ind].setAttribute("style","display: none");
-                          }
-                        });
-                      };
-
-                  </script>
-
-              <div class="col-md-4"><label for="doctor">Doctors:</label></div>
-                <div class="col-md-8">
-                    <select name="doctor" class="form-control" id="doctor" required="required">
-                      <option value="" disabled selected>Select Doctor</option>
-                
-                      <?php display_docs(); ?>
-                    </select>
-                  </div><br/><br/> 
-
-
-                        <script>
-              document.getElementById('doctor').onchange = function updateFees(e) {
-                var selection = document.querySelector(`[value=${this.value}]`).getAttribute('data-value');
-                document.getElementById('docFees').value = selection;
-              };
-            </script>
-
-                  
-                  
-
-                  
-                        <!-- <div class="col-md-4"><label for="doctor">Doctors:</label></div>
-                                <div class="col-md-8">
-                                    <select name="doctor" class="form-control" id="doctor1" required="required">
-                                      <option value="" disabled selected>Select Doctor</option>
-                                      
-                                    </select>
-                                </div>
-                                <br><br> -->
-
-                                <!-- <script>
-                                  document.getElementById("spec").onchange = function updateSpecs(event) {
-                                      var selected = document.querySelector(`[data-value=${this.value}]`).getAttribute("value");
-                                      console.log(selected);
-
-                                      var options = document.getElementById("doctor1").querySelectorAll("option");
-
-                                      for (i = 0; i < options.length; i++) {
-                                        var currentOption = options[i];
-                                        var category = options[i].getAttribute("data-spec");
-
-                                        if (category == selected) {
-                                          currentOption.style.display = "block";
-                                        } else {
-                                          currentOption.style.display = "none";
-                                        }
-                                      }
-                                    }
-                                </script> -->
-
-                        
-                    <!-- <script>
-                    let data = 
-                
-              document.getElementById('spec').onchange = function updateSpecs(e) {
-                let values = data.filter(obj => obj.spec == this.value).map(o => o.username);   
-                document.getElementById('doctor1').value = document.querySelector(`[value=${values}]`).getAttribute('data-value');
-              };
-            </script> -->
-
-
-                  
-                  <div class="col-md-4"><label for="consultancyfees">
-                                Consultancy Fees
-                              </label></div>
-                              <div class="col-md-8">
-                              <!-- <div id="docFees">Select a doctor</div> -->
-                              <input class="form-control" type="text" name="docFees" id="docFees" readonly="readonly"/>
-                  </div><br><br>
-
-                  <div class="col-md-4"><label>Appointment Date</label></div>
-                  <div class="col-md-8"><input type="date" class="form-control datepicker" name="appdate"></div><br><br>
-
-                  <div class="col-md-4"><label>Appointment Time</label></div>
-                  <div class="col-md-8">
-                    <!-- <input type="time" class="form-control" name="apptime"> -->
-                    <select name="apptime" class="form-control" id="apptime" required="required">
-                      <option value="" disabled selected>Select Time</option>
-                      <option value="08:00:00">8:00 AM</option>
-                      <option value="10:00:00">10:00 AM</option>
-                      <option value="12:00:00">12:00 PM</option>
-                      <option value="14:00:00">2:00 PM</option>
-                      <option value="16:00:00">4:00 PM</option>
-                    </select>
-
-                  </div><br><br>
-
-                  <div class="col-md-4">
-                    <input type="submit" name="app-submit" value="Create new entry" class="btn btn-primary" id="inputbtn">
-                  </div>
-                  <div class="col-md-8"></div>                  
-                </div>
-              </form>
-            </div>
+          <div class="row">
+            <div class="col-sm-4" style=" margin-left: 200px;">
+              <div class="panel panel-white no-radius text-center">
+                    <div class="panel-body" >
+                      <img src="images\QR.png" alt="Flowers in a" width="460" height="345">
+                      
+                    </div>
+              </div>
+            </div>                
           </div>
         </div><br>
       </div>
       
-<div class="tab-pane fade" id="app-hist" role="tabpanel" aria-labelledby="list-pat-list">
-        
-              <table class="table table-hover">
-                <thead>
-                  <tr>
-                    <th scope="col">Token Number</th>
-                    <th scope="col">Doctor Name</th>
-                    <th scope="col">Consultancy Fees</th>
-                    <th scope="col">Appointment Date</th>
-                    <th scope="col">Appointment Time</th>
-                    <th scope="col">Current Status</th>
-                    <th scope="col">Action</th>
-                    <th scope="col">QR</th>
 
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php 
-
-                    $con=mysqli_connect("localhost","root","","myhmsdb");
-                    global $con;
-
-                    $query = "select ID,doctor,docFees,appdate,apptime,userStatus,doctorStatus from appointmenttb where fname ='$fname' and lname='$lname';";
-                    $result = mysqli_query($con,$query);
-                    while ($row = mysqli_fetch_array($result)){
-              
-                      #$fname = $row['fname'];
-                      #$lname = $row['lname'];
-                      #$email = $row['email'];
-                      #$contact = $row['contact'];
-                  ?>
-                      <tr>
-                        <td><?php echo $row['ID'];?></td>
-                        <td><?php echo $row['doctor'];?></td>
-                        <td><?php echo $row['docFees'];?></td>
-                        <td><?php echo $row['appdate'];?></td>
-                        <td><?php echo $row['apptime'];?></td>
-                        
-                          <td>
-                    <?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
-                    {
-                      echo "Active";
-                    }
-                    if(($row['userStatus']==0) && ($row['doctorStatus']==1))  
-                    {
-                      echo "Cancelled by You";
-                    }
-
-                    if(($row['userStatus']==1) && ($row['doctorStatus']==0))  
-                    {
-                      echo "Cancelled by Doctor";
-                    }
-                        ?></td>
-
-                        <td>
-                        <?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
-                        { ?>
-
-													
-	                        <a href="admin-panel.php?ID=<?php echo $row['ID']?>&cancel=update" 
-                              onClick="return confirm('Are you sure you want to cancel this appointment ?')"
-                              title="Cancel Appointment" tooltip-placement="top" tooltip="Remove"><button class="btn btn-danger">Cancel</button></a>
-	                        <?php } else {
-
-                                echo "Cancelled";
-                                } ?>
-                        
-                        </td>
-                        <td>
-                          <a href="QR" button class="btn btn-danger" role="button">View</a></td>
-
-                      </tr>
-                    <?php } ?>
-                </tbody>
-              </table>
-        <br>
-      </div>
-
-
-
-      <div class="tab-pane fade" id="list-pres" role="tabpanel" aria-labelledby="list-pres-list">
-        
-              <table class="table table-hover">
-                <thead>
-                  <tr>
-                    
-                    <th scope="col">Doctor Name</th>
-                    <th scope="col">Appointment ID</th>
-                    <th scope="col">Appointment Date</th>
-                    <th scope="col">Appointment Time</th>
-                    <th scope="col">Diseases</th>
-                    <th scope="col">Allergies</th>
-                    <th scope="col">Prescriptions</th>
-                    <th scope="col">Bill Payment</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php 
-
-                    $con=mysqli_connect("localhost","root","","myhmsdb");
-                    global $con;
-
-                    $query = "select doctor,ID,appdate,apptime,disease,allergy,prescription from prestb where pid='$pid';";
-                    
-                    $result = mysqli_query($con,$query);
-                    if(!$result){
-                      echo mysqli_error($con);
-                    }
-                    
-
-                    while ($row = mysqli_fetch_array($result)){
-                  ?>
-                      <tr>
-                        <td><?php echo $row['doctor'];?></td>
-                        <td><?php echo $row['ID'];?></td>
-                        <td><?php echo $row['appdate'];?></td>
-                        <td><?php echo $row['apptime'];?></td>
-                        <td><?php echo $row['disease'];?></td>
-                        <td><?php echo $row['allergy'];?></td>
-                        <td><?php echo $row['prescription'];?></td>
-                        <td>
-                          <form method="get">
-                          <!-- <a href="admin-panel.php?ID=" 
-                              onClick=""
-                              title="Pay Bill" tooltip-placement="top" tooltip="Remove"><button class="btn btn-success">Pay</button>
-                              </a></td> -->
-
-                              <a href="admin-panel.php?ID=<?php echo $row['ID']?>">
-                              <input type ="hidden" name="ID" value="<?php echo $row['ID']?>"/>
-                              <input type = "submit" onclick="alert('Bill Paid Successfully');" name ="generate_bill" class = "btn btn-success" value="Pay Bill"/>
-                              </a>
-                              </td>
-                              </form>
-
-                    
-                      </tr>
-                    <?php }
-                    ?>
-                </tbody>
-              </table>
-        <br>
-      </div>
-
-
-
-
-      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
-        <form class="form-group" method="post" action="func.php">
-          <label>Doctors name: </label>
-          <input type="text" name="name" placeholder="Enter doctors name" class="form-control">
-          <br>
-          <input type="submit" name="doc_sub" value="Add Doctor" class="btn btn-primary">
-        </form>
-      </div>
-       <div class="tab-pane fade" id="list-attend" role="tabpanel" aria-labelledby="list-attend-list">...</div>
-    </div>
-  </div>
-</div>
    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
